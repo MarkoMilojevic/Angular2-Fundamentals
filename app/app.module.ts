d084b2d1dd0c1fbde9b5be8524c3033c5da02948
Marko Milojevic <milojevicmarko92@gmail.com>
2017-06-06 16:56:31 +0200

    Initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { EventsAppComponent } from "./events-app.component";
+import { EventsListComponent } from "./events/events-list.component";
+import { EventThumbnailComponent } from "./events/event-thumbnail.component";
+import { NavComponent } from "./nav/nav.component";
+
+@NgModule({
+    imports: [
+        BrowserModule
+    ],
+    declarations: [
+        EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        NavComponent
+    ],
+    bootstrap: [
+        EventsAppComponent
+    ]
+})
+export class AppModule { }
---
+++ app/events/events-list.component.ts
@@ -0,0 +1,62 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'events-list',
+    templateUrl: 'app/events/events-list.component.html'
+})
+export class EventsListComponent {
+    events: any = [
+        {
+            id: 1,
+            name: 'Angular Connect',
+            date: '9/26/2036',
+            time: '10:00 am',
+            price: 599.99,
+            imageUrl: 'app/assets/images/angularconnect-shield.png',
+            location: {
+                address: '1057 DT',
+                city: 'London',
+                country: 'England'
+            }
+        },
+        {
+            id: 1,
+            name: 'Angular Connect',
+            date: '9/26/2036',
+            time: '10:00 am',
+            price: 599.99,
+            imageUrl: 'app/assets/images/angularconnect-shield.png',
+            location: {
+                address: '1057 DT',
+                city: 'London',
+                country: 'England'
+            }
+        },
+        {
+            id: 1,
+            name: 'Angular Connect',
+            date: '9/26/2036',
+            time: '10:00 am',
+            price: 599.99,
+            imageUrl: 'app/assets/images/angularconnect-shield.png',
+            location: {
+                address: '1057 DT',
+                city: 'London',
+                country: 'England'
+            }
+        },
+        {
+            id: 1,
+            name: 'Angular Connect',
+            date: '9/26/2036',
+            time: '10:00 am',
+            price: 599.99,
+            imageUrl: 'app/assets/images/angularconnect-shield.png',
+            location: {
+                address: '1057 DT',
+                city: 'London',
+                country: 'England'
+            }
+        }
+    ]
+}
